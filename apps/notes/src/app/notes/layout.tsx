a2c1aff7bb9780ca4e9ffa51ad4ed519b4086e79
Alfredo Guida <a.guida@radicalstorage.com>
2025-07-31 08:58:30 +0200

    feat(layout): implement layout structure and styling for notes app

---
+++ apps/notes/src/app/notes/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Noto_Serif, Raleway } from "next/font/google";
+import "../globals.css";
+import { Providers } from "@/components/Providers";
+
+const raleway = Raleway({
+  variable: "--font-raleway-sans",
+  subsets: ["latin"],
+});
+
+const notoSerif = Noto_Serif({
+  variable: "--font-noto-serif",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Notes app",
+  description: "a challenge for keyless",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${notoSerif.variable} ${raleway.variable} antialiased`}>
+        <div className="font-sans flex  min-h-screen   gap-16 ">
+          <Providers>{children}</Providers>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ apps/notes/src/app/notes/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-full ">
+      <div className="flex flex-col p-8 gap-4 border-r flex-shrink border-accent h-full"></div>
+      <div className="flex flex-col p-8 gap-4 flex-grow"></div>
+    </div>
+  );
+}
